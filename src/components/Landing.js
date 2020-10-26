fb2e5fa829df081b4f1017ad7180dd100c79630b
sulaimanshub <sulaiman-123@hotmail.com>
2020-10-26 09:38:11 -0400

    brought link to encompass button

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -7,7 +7,7 @@ function Landing () {
     <h2>Need Tek support? We've got an army of Teknicians, mobilized, ready!</h2>
     <div className='call-to-action'>
       <button>Login</button>
-      <button><Link className='no-underline' to='/hiresignup'>New to Teknik.ly? Sign up now!</Link></button>
+      <Link className='no-underline' to='/hiresignup'><button>New to Teknik.ly? Sign up now!</button></Link>
       <br /><br /><br />
       <h2>
         Want work in your field? On your on <a href='/linkedin'>schedule</a>, and terms?
